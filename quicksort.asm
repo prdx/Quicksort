552b039bd86b3b5ae5811af1613eafeaeead48e3
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-01-11 13:46:27 -0500

    Finish print_array function

--- quicksort.asm
+++ quicksort.asm
@@ -58,7 +58,7 @@
     open_bracket:       .asciiz "["
     close_bracket:      .asciiz " ]\n"
 
-    space:      .char " "
+    space:      .asciiz " "
 
 .text
 main:
@@ -67,9 +67,16 @@ main:
   la    $a0, initial_array
   syscall
 
-  li    $v0, 4
-  la    $a0, names
-  syscall
+  # print_array(data, size);
+  la $a0, names
+  la    $a1, size
+  lw    $a1, 0($a1)
+  
+  addi $sp, $sp, -4
+  sw $ra, 0($sp)
+  jal print_array
+  lw $ra, 0($sp)
+  addi $sp, $sp, 4
 
 
   # exit(0)
@@ -77,26 +84,42 @@ main:
   li $a0, 0   # Return 0
   syscall
 
+# void print_array(const char * a[], const int size)
 print_array:
+  move $t0, $a0 # Transfer the array to another register
+  move $t1, $a1 # Transfer the array size to another register 
+
   # printf("[")
   li    $v0, 4
   la    $a0, open_bracket
   syscall
 
   # for(int i = 0, i < size, i++)
-  print_loop:
-    beq     $t0, $t1, end_loop
+  li $t2, 0
 
-    # TODO: printf(" %s", a[i]);
-    li $a0, " "
-    li $v0, 11
+  print_loop:
+    beq     $t2, $t1, end_loop # If t2 == size end loop
+
+    # printf(" %s", a[i]);
+    la $a0, space
+    li $v0, 4
     syscall
 
-  end_loop:
+    move $a0, $t0
+    li $v0, 4
+    syscall
+
+    addi    $t2, $t2, 1 # i++
+    addi    $t0, $t0, 32 # Advance the array pointer
+
+    j print_loop # Repeat
   
 
+  end_loop:    
+    # printf(" ]\n");
+    li    $v0, 4
+    la    $a0, close_bracket
+    syscall
 
-
-
-			
-	
+    li $v0, 0
+    jr $ra
